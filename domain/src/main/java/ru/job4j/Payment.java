ed88a32f18de8df346d0e9fd4a1980aed3c7a1c8
vlad <geraldik1986@gmail.com>
2022-11-29 22:53:01 +0300

    2. Монолитные системы [#338433]

---
+++ domain/src/main/java/ru/job4j/Payment.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Payment {
+    @EqualsAndHashCode.Include
+    private int id;
+    private String method;
+    private boolean successful;
+    private Timestamp paymentTime;
+}
