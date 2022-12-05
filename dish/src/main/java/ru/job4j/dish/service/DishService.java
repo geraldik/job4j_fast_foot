7d14fba17bb6f356dc23764dc97d66e4d851a454
vlad <geraldik1986@gmail.com>
2022-12-05 21:02:44 +0300

    Checkstyle

--- dish/src/main/java/ru/job4j/dish/service/DishService.java
+++ dish/src/main/java/ru/job4j/dish/service/DishService.java
@@ -1,19 +1,20 @@
 package ru.job4j.dish.service;
 
-
 import ru.job4j.domain.Dish;
 
 import java.util.List;
 
 public interface DishService {
 
-    void addDish(Dish dish);
+    Dish addDish(Dish dish);
+
+    boolean update(Dish dish);
 
     Dish findByName(String name);
 
     Dish findById(int id);
 
     List<Dish> findAll();
-    boolean deleteDish(Dish dish);
-}
 
+    boolean deleteById(int id);
+}
