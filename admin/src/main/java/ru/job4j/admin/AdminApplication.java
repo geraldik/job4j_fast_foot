4dbc8b289950af08e6c38b6574604c8fee7fcc99
vlad <geraldik1986@gmail.com>
2022-12-09 19:50:45 +0300

    Move RestTemplate Bean to config class

--- admin/src/main/java/ru/job4j/admin/AdminApplication.java
+++ admin/src/main/java/ru/job4j/admin/AdminApplication.java
@@ -3,22 +3,11 @@ package ru.job4j.admin;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
 @Slf4j
 public class AdminApplication {
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.setConnectTimeout(Duration.ofMillis(30000))
-                .setReadTimeout(Duration.ofMillis(30000)).build();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
---
+++ admin/src/main/java/ru/job4j/admin/config/HttpClientConfig.java
@@ -0,0 +1,18 @@
+package ru.job4j.admin.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+ @Configuration
+public class HttpClientConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.setConnectTimeout(Duration.ofMillis(30000))
+                .setReadTimeout(Duration.ofMillis(30000)).build();
+    }
+}
